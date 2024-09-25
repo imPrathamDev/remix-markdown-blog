024f2b3cd20e42d33f75954a12bb5acf71dc896e
Pratham <pratham.sharma2105@gmail.com>
2024-09-26 01:58:08 +0530

    update

--- app/components/home/CategorySection.tsx
+++ app/components/home/CategorySection.tsx
@@ -1,5 +1,7 @@
 import React, { useMemo } from "react";
 import TitleAnimation from "./TitleAnimation";
+import moment from "moment";
+import { Link, unstable_useViewTransitionState } from "@remix-run/react";
 
 function CategorySection({ blogs }: { blogs: any[] }) {
   const category = "Yoga";
@@ -8,6 +10,9 @@ function CategorySection({ blogs }: { blogs: any[] }) {
       (f: any) => f.data && f.data.categories.includes(category)
     );
   }, [blogs]);
+  const isTransitioning = unstable_useViewTransitionState(
+    "/blog/" + filteredPosts[0].id
+  );
   return (
     <section className="py-20">
       <div className="">
@@ -15,20 +20,38 @@ function CategorySection({ blogs }: { blogs: any[] }) {
           {category}{" "}
           <span className="text-lg font-light italic">(Category)</span>
         </h3>
-        <div className="w-3/5 h-[65vh] rounded-3xl overflow-hidden relative">
-          <img
-            src={filteredPosts[0].data.thumbnail}
-            alt={
-              filteredPosts[0].data.title +
-              " By " +
-              filteredPosts[0].data.author
-            }
-            className="w-full h-full object-cover rounded-3xl"
-          />
-          <div className="absolute w-full h-full top-0 left-0 p-12 flex items-end">
-            <TitleAnimation title={filteredPosts[0].data.title} />
+        <Link to={"/blog/" + filteredPosts[0].id} unstable_viewTransition>
+          <div className="w-3/5 h-[65vh] rounded-3xl overflow-hidden relative">
+            <img
+              style={{
+                ...(isTransitioning && {
+                  viewTransitionName: filteredPosts[0].id,
+                }),
+              }}
+              src={filteredPosts[0].data.thumbnail}
+              alt={
+                filteredPosts[0].data.title +
+                " By " +
+                filteredPosts[0].data.author
+              }
+              className="w-full h-full object-cover rounded-3xl"
+            />
+            <div className="absolute w-full h-full top-0 left-0 p-12 flex flex-col justify-between">
+              <div className="flex flex-col items-end w-full text-sm font-semibold gap-y-2">
+                <div className="px-4 py-2 bg-primary-white rounded-full">
+                  <span>
+                    {moment(filteredPosts[0].data.date).format("Do MMM YYYY")}
+                  </span>
+                </div>
+
+                <div className="px-4 py-2 bg-primary-white rounded-full">
+                  <span>{filteredPosts[0].data.author}</span>
+                </div>
+              </div>
+              <TitleAnimation title={filteredPosts[0].data.title} />
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </section>
   );
